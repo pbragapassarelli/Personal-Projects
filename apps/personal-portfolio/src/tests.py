213c887820bd8ae29f2cc03d036226b2062b1289
Pedro Passarelli <pbragapassarelli@yahoo.com.br>
2021-09-04 15:47:15 -0300

    Adding test for buying existing asset

--- apps/personal-portfolio/src/tests.py
+++ apps/personal-portfolio/src/tests.py
@@ -19,3 +19,14 @@ def test_buy_asset_for_first_time():
     portfolio.buy(ticker, qty)
 
     assert portfolio.assets[ticker].quantity == qty
+
+
+def test_buy_existing_asset():
+    portfolio = Portfolio()
+    ticker = 'ITSA4'
+    qty = 100
+    portfolio.buy(ticker, qty)
+    new_qty = 200
+    portfolio.buy(ticker, new_qty)
+
+    assert portfolio.assets[ticker].quantity == qty + new_qty
