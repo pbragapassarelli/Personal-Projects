900e014d3f4a8b58a36f2193aabbfc6e4b7f898b
Pedro Passarelli <pbragapassarelli@yahoo.com.br>
2021-12-14 20:18:49 -0300

    Linting

--- apps/personal-portfolio/src/app.py
+++ apps/personal-portfolio/src/app.py
@@ -1,8 +1,7 @@
 import os
+import time
 from dotenv import load_dotenv
 load_dotenv()
-
-import time
 
 API_KEY = os.getenv('API_KEY')
 MAX_REQUESTS_PER_MINUTE = 5
@@ -11,12 +10,14 @@ MAX_REQUESTS_PER_DAY = 500
 SECONDS_IN_A_MINUTE = 60
 
 MESSAGE_NOT_ON_PORTFOLIO = 'Asset is not on portfolio'
-MESSAGE_TRYING_TO_SELL_MORE_THAN_AVAILABLE = 'Trying to sell more quantity than available'
+MESSAGE_TRYING_TO_SELL_MORE_THAN_AVAILABLE = \
+    'Trying to sell more quantity than available'
+
 
 def get_price_for_ticker(ticker, key=API_KEY):
     '''
     Receives a ticker and returns the current price
-    
+
     ticker: str
     key: str
     ---------------------
@@ -48,7 +49,7 @@ class PortfolioAsset:
     '''
 
     def __init__(
-        self, 
+        self,
         ticker,
         quantity=0,
         amount_invested=0
@@ -58,7 +59,7 @@ class PortfolioAsset:
         self.average_price = amount_invested / quantity if quantity > 0 else 0
         self.amount_invested = amount_invested
         self.current_price = 0
-        self.current_exposition = self.current_price * self.quantity 
+        self.current_exposition = self.current_price * self.quantity
 
     def get_attributes(self):
         '''
@@ -73,7 +74,7 @@ class PortfolioAsset:
         }
 
     def update_price_and_exposition(self):
-        #TODO: Como testar?
+        # TODO: Como testar?
         self.current_price = get_price_for_ticker(self.ticker)
         self.current_exposition = self.current_price * self.quantity
 
@@ -89,7 +90,7 @@ class Portfolio:
         self.assets = []
 
     def _add_new_asset(self, ticker):
-        self.assets.append(PortfolioAsset(ticker)) 
+        self.assets.append(PortfolioAsset(ticker))
 
     def _get_asset_by_ticker(self, ticker):
         return next(filter(lambda x: x.ticker == ticker,  self.assets), None)
@@ -99,10 +100,10 @@ class Portfolio:
 
     def buy(self, ticker, quantity, price):
         if not self._has(ticker):
-            self._add_new_asset(ticker)     
-        
+            self._add_new_asset(ticker)
+
         asset = self._get_asset_by_ticker(ticker)
-        
+
         asset.quantity += quantity
         asset.amount_invested += quantity * price
         asset.average_price = asset.amount_invested / asset.quantity
@@ -119,14 +120,15 @@ class Portfolio:
         asset.quantity -= quantity
         asset.amount_invested -= quantity * asset.average_price
 
-        if asset.quantity == 0: #TODO: é responsabilidade do sell() remover o ativo da carteira?
+        if asset.quantity == 0:
+            # TODO: é responsabilidade do sell() remover o ativo da carteira?
             self.assets.remove(asset)
 
     def update(self):
-        #TODO: como testar?
+        # TODO: como testar?
         i = 0
         for asset in self.assets:
-            i+=1
+            i += 1
             asset.update_price_and_exposition()
             if i < len(self.assets):
                 time.sleep(SECONDS_IN_A_MINUTE/MAX_REQUESTS_PER_MINUTE)
